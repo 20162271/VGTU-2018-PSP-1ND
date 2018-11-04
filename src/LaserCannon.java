1d370de8e27a4ca9183d0db01b9b0a0a178e9865
MartynasArlauskas <44753773+MartynasArlauskas@users.noreply.github.com>
2018-11-04 20:53:15 +0200

    Add files via upload

---
+++ src/LaserCannon.java
@@ -0,0 +1,18 @@
+public class LaserCannon {
+    private int cannonPos;
+
+    public LaserCannon(int position) {
+        this.cannonPos = position;
+    }
+
+    public int getCannonPos() {
+        return cannonPos;
+    }
+
+    public void setCannonPos(int cannonPos) {
+        this.cannonPos = cannonPos;
+    }
+    
+    
+    
+}
